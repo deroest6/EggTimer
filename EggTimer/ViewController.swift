9e8488849ae0d3a63bb0fb3fa7477c48bec157cb
Jantsen <deroest6@hotmail.com>
2022-07-11 16:24:56 -0600

    Added comment to hardnessSelected func, first initial remote push

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,6 +9,7 @@ class ViewController: UIViewController {
     
     let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
 
+    //Targets the hardness selected and prints the time relative to each eggs hardness
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness = sender.currentTitle
 
