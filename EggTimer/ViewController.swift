f79aef5e7ae3d3edecbb1f166b3b957871a07db2
Jantsen <deroest6@hotmail.com>
2022-07-13 09:16:24 -0600

    Added timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -6,19 +6,49 @@ import UIKit
 class ViewController: UIViewController {
     
     let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
-
+    
+    var secondsRemaining = 60
     //User selects hardness and prints the time relative to each eggs hardness
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle
-
-        if eggTimes[hardness!] != nil {
-
-            let result = eggTimes[hardness!]!
-
-            print(result)
+        
+        print("Button Pressed")
+        
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.secondsRemaining > 0 {
+                print ("\(self.secondsRemaining) seconds")
+                self.secondsRemaining -= 1
+            } else {
+                Timer.invalidate()
+            }
         }
-
+        
+//        let hardness = sender.currentTitle
+//
+//        if eggTimes[hardness!] != nil {
+//
+//            let result = eggTimes[hardness!]!
+//
+//            print(result)
+//        }
+        
     }
     
 }
 
+//class ViewController: UIViewController {
+//
+//var secondsRemaining = 30
+//
+//@IBAction func startTimer(_ sender: UIButton) {
+//
+//    Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+//        if self.secondsRemaining > 0 {
+//            print ("\(self.secondsRemaining) seconds")
+//            self.secondsRemaining -= 1
+//        } else {
+//            Timer.invalidate()
+//        }
+//    }
+//
+// }
+//}
