9c9bac3a87ad4deda6355896fddbda8a581b0f60
Jantsen <deroest6@hotmail.com>
2022-07-21 23:43:20 -0600

    Update UI Update Design Update Code

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -5,20 +5,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
-
-    //User selects hardness and prints the time relative to each eggs hardness
+    let eggTimes = ["Soft": 3, "Medium": 5, "Hard": 7]
+    
+    var timer = Timer()
+    var buttonPressed = false
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    //A click selects hardness and prints the time relative to each egg selection timer
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle
-
-        if eggTimes[hardness!] != nil {
-
-            let result = eggTimes[hardness!]!
-
-            print(result)
+        
+        timer.invalidate()
+        
+        self.titleLabel.text = "Do you like Green Eggs and Ham?" //Relabels title label text from "Done"
+        
+        let hardness = sender.currentTitle //Gets label
+        let result = eggTimes[hardness!]! //Gets value from label
+        var timeLeft = result * 1 //Egg times converted to seconds
+        
+        //Invalidates timer when timeLeft hits 0, and updates title label to say "Done"
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if timeLeft > 0 {
+                print ("\(timeLeft) seconds")
+                timeLeft -= 1
+            } else {
+                self.timer.invalidate()
+                self.titleLabel.text = "Done"
+            }
         }
-
     }
     
 }
-
